e06a69125886c4b03d8292378ec0b0616ae51f93
Josef 'veloc1ty' Stautner <hello@veloc1ty.de>
2021-02-17 21:40:53 +0100

    First usable version

--- README.md
+++ README.md
@@ -2,71 +2,130 @@
 
 A go wrapper lib for the [lemon.markets](https://lemon.markets) WebSocket streams.
 
-## Example code
+## Installation
+
+Install using go get:
+
+```
+go get -u github.com/vlcty/lemon-markets-websocket
+```
+
+## Usage
+
+Lemon markets provides two streams:
+
+- Price updates (ticks) represented by `lemon.TickUpdate`
+- Quotes (Bid, Ask, Sizes, etc) represented by `lemon.QuoteUpdate`
+
+The lib itself uses channels to communicate with your application. You are responsible for the channels to be created, sized and closed after usage. Message parsing may hang if the library needs to wait for your consumer.
+
+Example code:
 
 ```go
 package main
 
 import (
 	"fmt"
+	"time"
+
 	"github.com/vlcty/lemon-markets-websocket"
 )
 
 func main() {
-	stream, streamError := lemon.NewLemonMarketsStream()
+	// Create the needed channels
+	tickChan := make(chan *lemon.Tick, 0)
+	quoteChan := make(chan *lemon.Quote, 0)
+	errChan := make(chan error, 0)
 
-	if streamError != nil {
-		fmt.Println("Faled to connect to lemon.markets")
-	} else {
-		stream.Subscribe("DE000TUAG000") // TUI AG
-		stream.Subscribe("US88160R1014") // Tesla
-		stream.Subscribe("LU0274208692") // Xtrackers MSCI World
+	// Create a stream struct for tick updates and pass in the needed channels
+	tickStream, tickErr := lemon.NewTickStream(tickChan, errChan)
 
-		processData := true
-
-		for processData {
-			select {
-			case error := <-stream.ErrorPipe():
-				fmt.Println(error)
-				processData = false
-
-			case priceUpdate := <-stream.MarketPricePipe():
-				fmt.Printf("ISIN: %s -> New market price: %.4f\n", priceUpdate.ISIN, priceUpdate.Price)
-
-			case quoteUpdate := <-stream.QuotePipe():
-				fmt.Printf("ISIN: %s -> New Quote: Bid=%.4f Ask=%.4f Bidsize=%.4f Asksize=%.4f\n", quoteUpdate.ISIN,
-					quoteUpdate.Bid, quoteUpdate.Ask, quoteUpdate.Bidsize, quoteUpdate.Asksize)
-			}
-		}
-
-		// Always call disconnect to ensure everything is disassembled properly
-		stream.Disconnect()
+	if tickErr != nil {
+		panic(tickErr)
 	}
+
+	// Create a stream struct for quote updates and pass in the needed channels
+	quoteStream, quoteErr := lemon.NewQuoteStream(quoteChan, errChan)
+
+	if quoteErr != nil {
+		panic(quoteErr)
+	}
+
+	// Define some ISINs
+	isins := []string{
+		"DE000TUAG000", // TUI AG
+		"LS000IGOLD01", // L&S gold commodity
+		"US00165C1045", // AMC entertainment
+	}
+
+	for _, isin := range isins {
+		// Subscribe to ticks
+		tickStream.Subscribe(isin)
+
+		// Subscribe to quotes
+		quoteStream.Subscribe(isin)
+	}
+
+	// Create a regular exit condition
+	timeToStop := time.After(time.Minute)
+
+	// Create a loop to process updates
+	processUpdates := true
+
+	for processUpdates {
+		select {
+		case err := <-errChan:
+			processUpdates = false
+
+			// An error occured. Identify which one
+			if err == lemon.ErrConnectionClosed {
+				fmt.Println("Connection closed by remote peer")
+			} else if err == lemon.ErrUnknownISIN {
+				fmt.Println("Unknown or invalid ISIN in subscription")
+				processUpdates = true // Override exit decision. It's not a critical one
+			} else {
+				fmt.Println("Unknown Error: ", err.Error())
+			}
+
+		case tick := <-tickChan:
+			// A tick update was received: Print all available fields
+			fmt.Printf("Tick: %s -> %.4f, Quantity: %d\n", tick.ISIN, tick.Price, tick.Quantity)
+
+		case quote := <-quoteChan:
+			// A quote update was received: Print all available fields
+			fmt.Printf("Quote: %s -> Bid = %.4f, Ask = %.4f, Bidsize: %d, Asksize: %d\n", quote.ISIN,
+				quote.Bid, quote.Ask, quote.Bidsize, quote.Asksize)
+
+		case <-timeToStop:
+			// Time to stop. Disconnect on our behalf
+			fmt.Println("It's time to stop")
+			processUpdates = false
+			tickStream.Disconnect()
+			quoteStream.Disconnect()
+		}
+	}
+
+	// Disconnect from the streams (not a must and does not hurt even if already closed)
+	tickStream.Disconnect()
+	quoteStream.Disconnect()
+
+	// Cleanup
+	close(tickChan)
+	close(quoteChan)
+	close(errChan)
+
+	for len(tickChan) > 0 {
+		<-tickChan
+	}
+
+	for len(quoteChan) > 0 {
+		<-quoteChan
+	}
+
+	for len(errChan) > 0 {
+		<-errChan
+	}
+
+	// Done
 }
 ```
-
-As you can see you receive your information over three channels:
-
-- `stream.ErrorPipe()` which returns `error`
-- `stream.MarketPricePipe()` which returns `LemonMarketsPriceUpdate`
-- `stream.QuotePipe()` which returns `LemonMarketsQuoteUpdate`
-
-Type definitions:
-
-```go
-type LemonMarketsPriceUpdate struct {
-	ISIN     string
-	Price    float64
-	Quantity float64
-}
-
-type LemonMarketsQuoteUpdate struct {
-	ISIN string
-	Bid  float64
-	Ask  float64
-	Bidsize float64
-	Asksize float64
-}
-```
-
-If you are developing while Land und Schwarz is closed you can fake inputs via the functions `GeneratePriceUpdate` and `GenerateQuoteUpdate`. See source code for more info.
--- lemon.go
+++ lemon.go
@@ -1,25 +1,75 @@
+/*
+MIT License
+
+Copyright (c) 2021 Josef 'veloc1ty' Stautner
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+// Package lemon provides easy access to the lemon.markets WebSocket streams
+//
+// They offer two streams: Ticks are price and quantity updates of trades and/or simple price updates for an instrument. Quotes are bid, ask, bidszie and asksizes updates for an instrument.
+//
+// An instrument is for example a security or a commodity and is identified by an International Securities Identification Number or short: ISIN
+//
+// Lang und Schwarz, Market Maker, Xetra
+//
+// Lemon.markets is hooked up to Lang und Schwarz Tradecenter, a market maker from germany. During business hours of Xetra, the digital exchange from the Frankfurt Stock Exchange, the spreads will not differ much. This is called "Referenzmarktprinzip". Xetra's business hours are currently Monday to Friday 09:00 until 17:30 CET.
+//
+// Currently Lang und Schwarz offers these opening hours:
+//         Monday to Friday: 07:30 until 23:00 CET
+//         Saturday: 10:00 until 13:00 CET
+//         Sunday: 17:00 until 19:00 CET
+//
+// It's no use to connect to lemon.markets outside the business hours. Also the connection to lemon.markets is automatically terminated after one hour. Design your application to withstand that!
+//
+// More about Xetra opening hours: https://www.xetra.com/xetra-en/trading/trading-calendar-and-trading-hours
+// More about Lang und Schwarz opening hours: https://www.ls-tc.de/de/handelszeiten
+//
+// Use of channels
+//
+// This library is using channels for the communication with your application. To be precise: It's using *your* channels. You are responsible for each channel! It's your decision if you use a buffered or unbuffered channel. It's your responsibility to open, close and empty them. Please make sure your receiver is fetching fast enough (< 20 seconds). Otherwise lemon.markets may close the stream.
 package lemon
 
 import (
+	"encoding/json"
 	"errors"
+	"strings"
+
 	"github.com/gorilla/websocket"
 )
 
 var (
-	ErrConnectFailed    error = errors.New("Can't connect to lemon markets")
-	ErrConnectionClosed error = errors.New("Lemon markets connection closed")
-	ErrTypeUnknown      error = errors.New("Type not known")
-)
+	// ErrConnectFailed is returned when the WebSocket connection failed
+	ErrConnectFailed error = errors.New("Can't connect to lemon markets")
 
-const (
-	CONNECTIONTYPE_PRICE                   string = "price"
-	CONNECTIONTYPE_QUOTE                   string = "quote"
-	ACTION_SUBSCRIBE                       string = "subscribe"
-	ACTION_UNSUBSCRIBE                     string = "subscribe"
-	SPECIFIER_WITH_QUANTITY                string = "with-quantity"
-	SPECIFIER_WITH_UNCOVERED               string = "with-uncovered"
-	SPECIFIER_WITH_QUANTITIY_AND_UNCOVERED string = "with-quantity-with-uncovered"
-	SPECIFIER_WITH_QUANTITY_WITH_PRICE     string = "with-quantity-with-price"
+	// ErrConnectionClosed is returned when an active WebSocket connection closed. All further processing is stopped.
+	ErrConnectionClosed error = errors.New("Lemon markets connection closed")
+
+	// ErrUnknownISIN is returned when a subscription for an invalid or unknown ISIN occured. This error does not stop message processing
+	ErrUnknownISIN error = errors.New("Invalid ISIN")
+
+	// ErrInvalidRequest is returned when an invalud request was detected
+	ErrInvalidRequest error = errors.New("Invalid request detected")
+
+	// ErrNotImplemented is returned when the returned update did not match any type. This should never occure.
+	ErrNotImplemented error = errors.New("Update type not implemented. You should never see this")
 )
 
 type lemonMarketSubscription struct {
@@ -28,125 +78,207 @@ type lemonMarketSubscription struct {
 	ISIN      string `json:"value"`
 }
 
-type PriceUpdate struct {
-	ISIN     string  `json:"isin"`
-	Price    float64 `json:"price"`
-	Quantity float64 `json:"quantity"`
+// Tick represents a price update.m
+type Tick struct {
+	ISIN     string  `json:"isin"`     // ISIN of the instrument
+	Price    float64 `json:"price"`    // Current market price
+	Quantity uint    `json:"quantity"` // The quantity of the trade. If 0 then there was no actual trade but a simple price update
 }
 
-type QuoteUpdate struct {
-	ISIN    string  `json:"isin"`
-	Bid     float64 `json:"bid_price"`
-	Ask     float64 `json:"ask_price"`
-	Bidsize float64 `json:"bid_quan"`
-	Asksize float64 `json:"ask_quan"`
+// Quote represents a quote update.
+type Quote struct {
+	ISIN    string  `json:"isin"`      // ISIN of the instrument
+	Bid     float64 `json:"bid_price"` // Current bid price
+	Ask     float64 `json:"ask_price"` // Current ask price
+	Bidsize uint    `json:"bid_quan"`  // Current bid size
+	Asksize uint    `json:"ask_quan"`  // Current ask size
 }
 
-func (lmpr PriceUpdate) WasTrade() bool {
-	return lmpr.Quantity > 0
+type stream struct {
+	connection      *websocket.Conn
+	processData     bool
+	subscriptions   map[string]uint
+	getUpdateType   func() interface{}
+	sendUpdate      func(interface{})
+	getWebsocketUrl func() string
+	getSubscription func() *lemonMarketSubscription
+
+	errorChannel chan<- error
+	rawMessages  chan<- []byte
 }
 
-type OnPriceUpdateFunc func(PriceUpdate)
-type OnQuoteUpdateFunc func(QuoteUpdate)
-type OnErrorFunc func(error)
-
-type LemonMarketsStream struct {
-	connection     *websocket.Conn
-	connectiontype string
-	processData    bool
-	subscriptions  map[string]int
-	OnPriceUpdate  OnPriceUpdateFunc
-	OnQuoteUpdate  OnQuoteUpdateFunc
-	OnError        OnErrorFunc
+// TickStream streams ticks for the subscribed securities
+type TickStream struct {
+	stream
+	updateChannel chan<- *Tick
 }
 
-func (lms *LemonMarketsStream) Subscribe(isin string) {
+// QuoteStream streams quotes for the subscribed securities
+type QuoteStream struct {
+	stream
+	updateChannel chan<- *Quote
+}
+
+// NewTickStream will initalize a new connection to stream ticks. Keep in mind: You are responsible for the passed channels. Raises an error if the connection fails.
+func NewTickStream(updateChan chan<- *Tick, errChan chan<- error) (*TickStream, error) {
+	stream := &TickStream{}
+	stream.errorChannel = errChan
+	stream.updateChannel = updateChan
+
+	stream.getUpdateType = func() interface{} {
+		return &Tick{}
+	}
+
+	stream.sendUpdate = func(update interface{}) {
+		stream.updateChannel <- update.(*Tick)
+	}
+
+	stream.getWebsocketUrl = func() string {
+		return "wss://api.lemon.markets/streams/v1/marketdata"
+	}
+
+	stream.getSubscription = func() *lemonMarketSubscription {
+		return &lemonMarketSubscription{
+			Action:    "subscribe",
+			Specifier: "with-quantity-with-uncovered"}
+	}
+
+	err := stream.connect()
+
+	return stream, err
+}
+
+// NewQuoteStream will initalize a new connection to stream quotes. Keep in mind: You are responsible for the passed channels. Raises an error if the connection fails.
+func NewQuoteStream(updateChan chan<- *Quote, errChan chan<- error) (*QuoteStream, error) {
+	stream := &QuoteStream{}
+	stream.errorChannel = errChan
+	stream.updateChannel = updateChan
+
+	stream.getUpdateType = func() interface{} {
+		return &Quote{}
+	}
+
+	stream.sendUpdate = func(update interface{}) {
+		stream.updateChannel <- update.(*Quote)
+	}
+
+	stream.getWebsocketUrl = func() string {
+		return "wss://api.lemon.markets/streams/v1/quotes"
+	}
+
+	stream.getSubscription = func() *lemonMarketSubscription {
+		return &lemonMarketSubscription{
+			Action:    "subscribe",
+			Specifier: "with-quantity-with-price"}
+	}
+
+	err := stream.connect()
+
+	return stream, err
+}
+
+// Subscribe to an instrument by supplying an ISIN. Double subscriptions are prevented silently.
+func (lms *stream) Subscribe(isin string) {
 	if _, exists := lms.subscriptions[isin]; !exists {
 		lms.subscriptions[isin] = 1
 
-		switch lms.connectiontype {
-		case CONNECTIONTYPE_PRICE:
-			lms.connection.WriteJSON(&lemonMarketSubscription{
-				Action:    ACTION_SUBSCRIBE,
-				Specifier: SPECIFIER_WITH_QUANTITIY_AND_UNCOVERED,
-				ISIN:      isin})
+		subscription := lms.getSubscription()
+		subscription.ISIN = isin
 
-		case CONNECTIONTYPE_QUOTE:
-			lms.connection.WriteJSON(&lemonMarketSubscription{
-				Action:    ACTION_SUBSCRIBE,
-				Specifier: SPECIFIER_WITH_QUANTITY_WITH_PRICE,
-				ISIN:      isin})
-		}
+		lms.connection.WriteJSON(subscription)
 	}
 }
 
-func (lms *LemonMarketsStream) Unsubscribe(isin string) {
+// Unsubscribe to an instrument by supplying an ISIN. Double unsubscriptions are prevented silently.
+func (lms *stream) Unsubscribe(isin string) {
 	if _, exists := lms.subscriptions[isin]; exists {
 		delete(lms.subscriptions, isin)
 
 		lms.connection.WriteJSON(&lemonMarketSubscription{
-			Action: ACTION_UNSUBSCRIBE,
+			Action: "unsubscribe",
 			ISIN:   isin})
 	}
 }
 
-func Connect(connectiontype string) (*LemonMarketsStream, error) {
-	lms := &LemonMarketsStream{}
-	lms.connectiontype = connectiontype
-	var url string
-
-	switch connectiontype {
-	case CONNECTIONTYPE_PRICE:
-		url = "wss://api.lemon.markets/streams/v1/marketdata"
-
-	case CONNECTIONTYPE_QUOTE:
-		url = "wss://api.lemon.markets/streams/v1/quotes"
-
-	default:
-		return nil, ErrTypeUnknown
-	}
-
-	connection, _, connectionError := websocket.DefaultDialer.Dial(url, nil)
+func (lms *stream) connect() error {
+	connection, _, connectionError := websocket.DefaultDialer.Dial(lms.getWebsocketUrl(), nil)
 
 	if connectionError != nil {
-		return nil, ErrConnectFailed
+		return ErrConnectFailed
 	} else {
 		lms.connection = connection
 		lms.processData = true
-		lms.subscriptions = make(map[string]int)
+		lms.subscriptions = make(map[string]uint)
 
-		return lms, nil
+		go lms.listen()
+
+		return nil
 	}
 }
 
-func (lms *LemonMarketsStream) handleError(err error) {
+// Disconnect will disconnect from the WebSocket. Disconnect will not raise an error when the connection was already terminated.
+func (lms *stream) Disconnect() {
 	lms.processData = false
-
-	if lms.OnError != nil {
-		lms.OnError(err)
-	}
-
 	lms.connection.Close()
 }
 
-func (lms *LemonMarketsStream) ListenToUpdates() {
-	for lms.processData {
-		switch lms.connectiontype {
-		case CONNECTIONTYPE_PRICE:
-			update := PriceUpdate{}
-			if parseError := lms.connection.ReadJSON(&update); parseError != nil {
-				lms.handleError(parseError)
-			} else if lms.OnPriceUpdate != nil {
-				lms.OnPriceUpdate(update)
+func (lms *stream) listen() {
+	for {
+		update := lms.getUpdateType()
+
+		_, msg, err := lms.connection.ReadMessage()
+
+		if !lms.processData {
+			// While waiting for a message the connection cosed or user does not want to continue. Exit this routine!
+			return
+		} else if err != nil {
+			lms.processData = false
+
+			if websocket.IsCloseError(err, websocket.CloseGoingAway, websocket.CloseNormalClosure, websocket.CloseAbnormalClosure) {
+				lms.errorChannel <- ErrConnectionClosed
+			} else {
+				lms.errorChannel <- err
+			}
+		} else if isUnknownISIN(msg) {
+			lms.errorChannel <- ErrUnknownISIN
+		} else if isInvalidRequest(msg) {
+			lms.errorChannel <- ErrInvalidRequest
+		} else {
+			if lms.rawMessages != nil {
+				lms.rawMessages <- msg
 			}
 
-		case CONNECTIONTYPE_QUOTE:
-			update := QuoteUpdate{}
-			if parseError := lms.connection.ReadJSON(&update); parseError != nil {
-				lms.handleError(parseError)
-			} else if lms.OnQuoteUpdate != nil {
-				lms.OnQuoteUpdate(update)
+			var decodeError error
+
+			switch update.(type) {
+			case *Tick:
+				decodeError = json.Unmarshal(msg, update.(*Tick))
+
+			case *Quote:
+				decodeError = json.Unmarshal(msg, update.(*Quote))
+
+			default:
+				decodeError = ErrNotImplemented
+			}
+
+			if decodeError != nil {
+				lms.errorChannel <- decodeError
+			} else {
+				lms.sendUpdate(update)
 			}
 		}
 	}
+}
+
+// SetRawMessageChannel will take a channel where raw, untouched messages from the WebSocket will be sent into. Keep in mind that you are the one in charge of maintaining the channel.
+func (lms *stream) SetRawMessageChannel(channel chan<- []byte) {
+	lms.rawMessages = channel
+}
+
+func isUnknownISIN(message []byte) bool {
+	return strings.Contains(string(message), "This instrument does not exist")
+}
+
+func isInvalidRequest(message []byte) bool {
+	return strings.Contains(string(message), "Invalid request")
 }
